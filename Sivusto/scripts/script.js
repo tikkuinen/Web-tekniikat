5b97a83969e845ec00f328ac216dc4932f607d95
tikkuinen <titunen@gmail.com>
2022-05-15 13:38:02 +0300

    layout-korjauksia

--- Sivusto/scripts/script.js
+++ Sivusto/scripts/script.js
@@ -1,6 +1,20 @@
-fetch('http://numbersapi.com/random/trivia?json')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('api-here').innerHTML = data.text;
+getApi();
 
-    });
+// siivoa jos et laita nappia
+
+// let apiButton = document.getElementById("api-button");
+// apiButton.addEventListener("click", getApi);
+
+function getApi() {
+    fetch('http://numbersapi.com/random/trivia?json')
+        .then(response => response.json())
+        .then(data => {
+            let apiText = data.text;
+            
+            if (apiText.length < 100) {
+                document.getElementById('api-here').innerHTML = apiText;
+            } else {
+                getApi();
+            }
+        });    
+}
