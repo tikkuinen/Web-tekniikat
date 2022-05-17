abd03140d8f69fa56810d300768907f910d5dd58
tikkuinen <titunen@gmail.com>
2022-05-17 20:36:27 +0300

    kuva, fontteja ja värejä toteutettu

--- Sivusto/scripts/script.js
+++ Sivusto/scripts/script.js
@@ -1,9 +1,5 @@
+// Ensimmäisellä sivun latauksella haetaan fakta, ja jos merkkimäärä on liian pitkä, haetaan uudelleen.
 getApi();
-
-// siivoa jos et laita nappia
-
-// let apiButton = document.getElementById("api-button");
-// apiButton.addEventListener("click", getApi);
 
 function getApi() {
     fetch('http://numbersapi.com/random/trivia?json')
@@ -18,3 +14,6 @@ function getApi() {
             }
         });    
 }
+
+
+// siivoa jos et laita nappia ja katso tuo merkkimäärä kuntoon
